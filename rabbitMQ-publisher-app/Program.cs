39978f388a5217ad72f4f2e8041fd0348a4c8b29
Robson <robsonhobbye@gmail.com>
2024-08-08 13:37:45 -0300

    Adicionar arquivos de projeto.

---
+++ rabbitMQ-publisher-app/Program.cs
@@ -0,0 +1,3 @@
+﻿using rabbitMQ_publisher_app.Services;
+
+await MessageService.PublishMessageAsync();
---
+++ rabbitMQ-publisher-app/Services/MessageService.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace rabbitMQ_publisher_app.Services
+{
+    public static class MessageService
+    {
+        public static async Task PublishMessageAsync()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "test", durable: false, exclusive: false,
+                    autoDelete: false, arguments: null);
+
+                var body = Encoding.UTF8.GetBytes("Teste mensagem");
+
+                channel.BasicPublish(exchange: string.Empty,
+                    routingKey: "test", basicProperties: null, body: body);
+            }
+        }
+    }
+}
